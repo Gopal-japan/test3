0393566256ed4e0ea1af8c8768339fa47a8b9a61
Gopal-japan <Gopal-japan@users.noreply.github.com>
2024-11-26 05:22:23 +0000

    deploy: a0494c88a3d70b6a7e582246119c0b6b169094eb

---
+++ script.js
@@ -0,0 +1,14 @@
+// Function to add two numbers
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Function to subtract two numbers
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Export the functions so that they can be tested
+  module.exports = { add, subtract };
+  
+  
